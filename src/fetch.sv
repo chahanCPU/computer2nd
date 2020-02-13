1d89dbc1f77d048b5c4d734a9fe922375930c9eb
omosan0627 <hodakaymj0627@yahoo.co.jp>
2020-02-13 18:39:52 +0900

    分散RAMに戻した

--- src/fetch.sv
+++ src/fetch.sv
@@ -15,23 +15,54 @@ module fetch #( parameter CLK_PER_HALF_BIT = 434)
 	localparam EXEC = 2;
 
 
-	logic [INST_SIZE-1:0] addra;
-	logic [31:0] dina;
-	logic wea;
 
-	assign dina = 0;
-	assign wea = 0;
-	assign done = 1;
-	
-	assign addra = pc[INST_SIZE+1:2];
+	logic [INST_SIZE-1:0] pc_main;
+
+	reg [31:0] inst_mem[(2 ** INST_SIZE - 1) : 0];
+
+
+	assign inst = inst_mem[pc_main];
+
+	logic [INST_SIZE - 1 : 0] addra;
+	logic [INST_SIZE - 1 : 0] ppaddra;
+	logic [31:0] dina;
+	logic [0:0] wea;
+	logic [31:0] douta;
+
+	logic [1:0] latancy;
 
 	INST_BRAM _INST_BRAM (
 		.addra (addra),
 		.dina (dina),
 		.wea (wea),
 		.clka (clk),
-		.douta (inst));
+		.douta (douta));
 
+	always @(posedge clk) begin
+		if(~rstn) begin
+			done <= 0;
+			latancy <= 0;
+			addra <= 0;
+			ppaddra <= 0;
+			dina <= 0;
+			wea <= 0;
+		end
+		else begin
+			if(mode == LOAD) begin
+				if(ppaddra == (2 ** INST_SIZE - 2)) begin
+					done <= 1;
+				end
+				if(addra >= 2) begin
+					inst_mem[ppaddra] <= douta;
+				end
+				if(addra >= 2) ppaddra <= ppaddra + 1;
+				addra <= addra + 1;
+			end
+			else begin
+				pc_main <= pc[INST_SIZE+1:2];
+			end
+		end
+	end
 
 endmodule
 `default_nettype wire
